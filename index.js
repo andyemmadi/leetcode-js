429ef34d0d83ea61eb97da81927b144100dd73cf
andyemmadi <emmadi789@gmail.com>
2022-06-05 18:39:19 -0500

    2 files modified

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>LeetCode Challenges</h1>`;
+// appDiv.innerHTML = `<h1 >LeetCode Challenges</h1>`;
 
 const init = () => {
   console.log('init');
